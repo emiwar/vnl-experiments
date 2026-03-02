8c4b4901c7f5bb5b099ea7264b2db17605bd6f66
Emil Warnberg <ewarnberg@g.harvard.edu>
2026-03-02 06:25:58 -0500

    SLURM batch script.

---
+++ slurm_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -J rodentImitation
+#SBATCH -p gpu,gpu_h200 -t 3-00:00 --mem=64000 -c 4 -o slurm_logs/%j.out -e slurm_logs/%j.err --gres=gpu
+
+source /n/holylfs06/LABS/olveczky_lab/Users/ewarnberg/python_venvs/jax_etc/bin/activate
+srun python modular/dense_mlp.py
+
+
